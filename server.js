865ce1cce1e6450cd8c4733ffb612fcc635a6739
Laura Senko <laurasenko@gmail.com>
2017-08-30 15:57:19 -0500

    array change

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + "/public"));
 //puts pun array from puns.js into /pun path
 var puns = require("./puns");
 app.get('/puns', function (req, res) {
-	res.send(puns);	
+	res.send(puns.puns);	
 });
 
 //runs randomPun function and sends info to /random_pun path
@@ -18,7 +18,47 @@ function randomPun() {
 app.get('/random-pun', function (req, res) {
 	res.send(randomPun());
 });
+	
+/*var nextId = 1;
+function getNextId () {
+	return nextId++;
+}
 
+var tacos = [
+	{ id: getNextId(), type: "American", fillings: ["beef", "lettuce", "cheese", "salsa"]},
+	{ id: getNextId(), type: "Al Pastor", fillings: ["al pastor", "onion", "cilantro", "tomatoes"]}
+];
+
+app.get('/api/tacos', function (req, res) {
+	res.send(tacos);
+});
+
+app.get('/api/tacos/:id', function (req, res) {
+	var id = parseInt(req.params.id);
+	var foundTaco = tacos.find(function(taco) { //<--like a for each loop but returns a value
+		return taco.id ===id
+	});
+
+	if (foundTaco) {
+	res.send(tacos[id]);
+	} else {
+		res.status(404);
+		res.send("Taco not found");
+	}
+});
+
+
+app.delete('/api/tacos/:id', function (req, res) {
+	var id = parseInt(req.params.id);
+	var tacoIndex = tacos.findIndex(function (taco) {
+
+	})
+
+app.post('/api/tacos/, function (req, res) {
+	console.log(req.body);
+
+});
+*/
 // Start the server.
 var server = app.listen(3000, function () {
   var port = server.address().port;
